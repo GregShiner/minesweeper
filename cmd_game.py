20f52459001197f0c92e79a3b9fa0f7f6ea8dfa4
andrewcoale <andrewfc331@gmail.com>
2024-04-23 14:48:17 -0400

    Implemented replay functionality in cmd_game

--- cmd_game.py
+++ cmd_game.py
@@ -1,7 +1,7 @@
 from board import Board
 
 board = Board(10, 10)
-print(board)
+print("Welcome to Minesweeper!")
 
 
 def validate_square(r, c):
@@ -29,25 +29,57 @@ def win():
     print("You win! All unrevealed tiles remaining are mines")
 
 
-while True:
-    command = input("Reveal or flag a tile? ")
-    if command.lower() == 'reveal' or command.lower() == 'r':
-        row = input("Which row? (0 - " + str(board.size - 1) + ") ")
-        col = input("Which column? (0 - " + str(board.size - 1) + ") ")
-        if validate_square(row, col):
-            if not board.left_click(int(row), int(col)):
-                lose(int(row), int(col))
-                break
-            elif board.num_revealed + board.num_mines == board.size * board.size:
-                win()
-                break
-            else:
+def startGame():
+    print(board)
+
+    while True:
+        command = input("Reveal or flag a tile? ")
+        if command.lower() == 'reveal' or command.lower() == 'r':
+            row = input("Which row? (0 - " + str(board.size - 1) + ") ")
+            col = input("Which column? (0 - " + str(board.size - 1) + ") ")
+            if validate_square(row, col):
+                if not board.left_click(int(row), int(col)):
+                    lose(int(row), int(col))
+                    break
+                elif board.num_revealed + board.num_mines == board.size * board.size:
+                    win()
+                    break
+                else:
+                    print(board)
+        elif command.lower() == 'flag' or command.lower() == 'f':
+            row = input("Which row? (0 - " + str(board.size - 1) + ") ")
+            col = input("Which column? (0 - " + str(board.size - 1) + ") ")
+            if validate_square(row, col):
+                board.right_click(int(row), int(col))
                 print(board)
-    elif command.lower() == 'flag' or command.lower() == 'f':
-        row = input("Which row? (0 - " + str(board.size - 1) + ") ")
-        col = input("Which column? (0 - " + str(board.size - 1) + ") ")
-        if validate_square(row, col):
-            board.right_click(int(row), int(col))
-            print(board)
+        else:
+            print("Invalid command")
+
+
+while True:
+    size = input("How many rows/columns would you like? (x to escape) ")
+    if size.isdigit():
+        mines = input("How many mines would you like? ")
+        if mines.isdigit():
+            size = int(size)
+            mines = int(mines)
+            if mines < size * size:
+                board = Board(size, mines)
+                startGame()
+
+                cont = input("Play again? (y/n) ")
+                if cont == 'y':
+                    print("OK!")
+                elif cont == 'n':
+                    print("Ok, terminating...")
+                else:
+                    print("Input invalid, terminating...")
+            else:
+                print("Too many mines for board size")
+        else:
+            print("Invalid input")
+    elif size == 'x':
+        print("Terminating...")
+        break
     else:
-        print("Invalid command")
+        print("Invalid input")
